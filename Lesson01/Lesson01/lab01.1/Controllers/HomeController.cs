0cb5becc8a12f75813f6f69739bc3e71eeba1c6c
Nemnewcode <mrhien19792017@gmail.com>
2024-04-22 14:24:30 +0700

    Lesson01

---
+++ Lesson01/Lesson01/lab01.1/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab01._1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //sử dụng viewBag để đưa dữ liệu ra view 
+            ViewBag.message = "chào mừng bạn đến với ASP.NET MVC 5";
+            return View();
+        }
+        public ActionResult About()
+        {
+            //sử dụng viewBag để đưa dữ liệu ra view 
+            ViewBag.message = "DĐây là trang about";
+            return View();
+            
+        }
+    }
+}
