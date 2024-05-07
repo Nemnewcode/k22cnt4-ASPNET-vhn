1e08bf7295119a32afa26a488fe5c2a34e5f68ef
Nemnewcode <mrhien19792017@gmail.com>
2024-05-08 00:07:10 +0700

    lesson02_VHN

---
+++ lesson02_VHN/lesson02_VHN/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lesson02_VHN.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            ViewBag.name = "Vu Hoai Nam - 2210900050";
+            return View();
+        }
+
+        public ActionResult Details(int? id)
+        {
+            ViewBag.id = id;
+            return View();
+        }
+        public string Getname()
+        {
+            return "Vu Hoai Nam - 2210900050";
+        }
+        public JsonResult Listname()
+        {
+            string[] names = { "Trinh", "Nguyen", "Phuc", "Truong" };
+            return Json(names, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ lesson02_VHN/lesson02_VHN/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="lesson02_VHN.MvcApplication" Language="C#" %>
